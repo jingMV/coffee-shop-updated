701f4637180650aa9424cc5f014c91d920b2d48b
jingMV <genesisviado123@gmail.com>
2019-05-01 16:44:52 +0700

    Initial commit adding div class

---
+++ main.js
@@ -0,0 +1,23 @@
+//nav
+function openNav() {
+    var nav = document.getElementById("nav");
+    nav.classList.toggle("navContent");
+}
+function closeNav() {
+    var nav = document.getElementById("nav");
+    nav.classList.toggle("navContent");
+}
+//product animation
+window.onscroll = yHandler;
+function yHandler() {
+
+    var wh = window.innerHeight;
+    var h = window.pageYOffset;
+    var newHeight = h + wh;
+
+    if (newHeight > 1450) {
+        document.getElementById("product-container").style.display = "block";
+    }
+    console.log(newHeight);
+}
+
